c26a2eff8790ee39eccd5f6c732e182d47ca479c
tabo-syu <takeshima.nitkc@gmail.com>
2023-03-09 14:10:36 +0000

    証明書の分岐を追加

--- infrastructures/sql_client.go
+++ infrastructures/sql_client.go
@@ -2,22 +2,35 @@ package infrastructures
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func NewSQLHandler() (*sql.DB, error) {
-	handler, err := sql.Open("pgx", fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable Timezone=%s",
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s Timezone=%s",
 		os.Getenv("POSTGRES_HOSTNAME"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
 		os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_TIMEZONE"),
-	))
+	)
+	if _, err := os.Stat("root.crt"); !errors.Is(err, os.ErrNotExist) {
+		// 証明書が存在しているとき
+		dsn = fmt.Sprintf("%s sslmode=verify-full sslrootcert=root.crt", dsn)
+		log.Println("sslmode=verify-full")
+	} else {
+		// 証明書がないとき
+		dsn = fmt.Sprintf("%s sslmode=disable", dsn)
+		log.Println("sslmode=disable")
+	}
+
+	handler, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("incorrect DB connection information: %w", err)
 	}
